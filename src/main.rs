340852cc9bd33b14fa20a3691701448ef577ad9e
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-09-13 17:32:56 +0200

    Respect cli parameters
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 extern crate docopt;
 extern crate futures;
+extern crate krankerl;
 #[macro_use]
 extern crate serde_derive;
 extern crate tokio_core;
-extern crate krankerl;
 
 use docopt::Docopt;
 use futures::future::Future;
@@ -25,20 +25,25 @@ Options:
 #[derive(Debug, Deserialize)]
 struct Args {
     arg_version: String,
+    cmd_list: bool,
+    cmd_apps: bool,
 }
 
 fn main() {
     let args: Args = Docopt::new(USAGE)
-                            .and_then(|d| d.deserialize())
-                            .unwrap_or_else(|e| e.exit());
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
 
     let mut core = Core::new().unwrap();
-    let work = get_apps_and_releases(&core.handle(), &args.arg_version).map(|apps| {
-        println!("found {} apps:", apps.len());
-        for app in apps {
-            println!("{:?}", app)
-        }
-    });
 
-    core.run(work).unwrap();
+    if args.cmd_list && args.cmd_apps {
+        let work = get_apps_and_releases(&core.handle(), &args.arg_version).map(|apps| {
+            println!("found {} apps for {}:", apps.len(), args.arg_version);
+            for app in apps {
+                println!("- {}", app.id)
+            }
+        });
+
+        core.run(work).unwrap();
+    }
 }
