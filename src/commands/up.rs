9566d6be8c1704a9e2b8f2ed08fa1c64328e5960
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-05-14 22:10:09 +0200

    Reuse progress bar style
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/commands/up.rs
+++ src/commands/up.rs
@@ -8,11 +8,6 @@ use npm_scripts::NpmScripts;
 
 fn npm_up(app_path: &PathBuf, pb: ProgressBar) -> Result<(), Error> {
     pb.enable_steady_tick(200);
-    pb.set_style(
-        ProgressStyle::default_spinner()
-            .tick_chars("/|\\- ")
-            .template("{spinner:.dim.bold} {wide_msg}"),
-    );
 
     let npm_script = "build".to_owned();
     let scripts = NpmScripts::new(app_path);
@@ -38,11 +33,6 @@ fn npm_up(app_path: &PathBuf, pb: ProgressBar) -> Result<(), Error> {
 
 fn composer_up(app_path: &PathBuf, pb: ProgressBar) -> Result<(), Error> {
     pb.enable_steady_tick(200);
-    pb.set_style(
-        ProgressStyle::default_spinner()
-            .tick_chars("/|\\- ")
-            .template("{spinner:.dim.bold} {wide_msg}"),
-    );
 
     let composer = Composer::new(app_path);
     if composer.is_available() {
@@ -57,11 +47,16 @@ fn composer_up(app_path: &PathBuf, pb: ProgressBar) -> Result<(), Error> {
 
 pub fn up(app_path: &PathBuf) -> Result<(), Error> {
     let m = MultiProgress::new();
+    let sty = ProgressStyle::default_spinner()
+        .tick_chars("/|\\- ")
+        .template("{spinner:.dim.bold} {wide_msg}");
 
     let pb = m.add(ProgressBar::new_spinner());
+    pb.set_style(sty.clone());
     let p1 = app_path.to_owned();
     let t1 = thread::spawn(move || npm_up(&p1, pb));
     let pb = m.add(ProgressBar::new_spinner());
+    pb.set_style(sty.clone());
     let p2 = app_path.to_owned();
     let t2 = thread::spawn(move || composer_up(&p2, pb));
 
