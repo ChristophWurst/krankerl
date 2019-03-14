a9207ebf53d77af327d16a64280950a32179d6a1
Christoph Wurst <christoph@winzerhof-wurst.at>
2019-03-14 19:41:46 +0100

    Ignore files by .nextcloudignore
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ extern crate futures;
 extern crate globset;
 extern crate hex;
 extern crate git2;
+extern crate ignore;
 extern crate indicatif;
 extern crate nextcloud_appinfo;
 extern crate nextcloud_appsignature;
